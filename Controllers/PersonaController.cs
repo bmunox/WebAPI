d1275090a4de344aac5e1300f9cf6d6ef837d739
Bryan Munoz <brimunvi@hotmail.com>
2021-04-20 18:55:18 -0600

    webAPI

---
+++ Controllers/PersonaController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+using Newtonsoft.Json;
+
+namespace WebAPI.Controllers
+{
+    public class PersonaController : ApiController
+    {
+        [HttpGet]
+        [Route("api/Persona/obtener/{id}")]
+        public HttpResponseMessage ObtenerPersonas(int id) {
+            List<Persona> lista = new List<Persona>();
+            lista.Add(new Persona(1, "115510467", "Jason Bonilla"));
+            lista.Add(new Persona(2, "123456789", "Bryan"));
+
+            return Request.CreateResponse(HttpStatusCode.OK, JsonConvert.SerializeObject(lista));
+        }
+
+        [HttpPost]
+        [Route("api/Persona/datos")]
+        public HttpResponseMessage PostPersona(Persona persona)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, JsonConvert.SerializeObject(new Persona(1, "115510467", "Jason Bonilla")));
+        }
+
+
+    }
+}
---
+++ Models/Persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class Persona
+    {
+        public int ID { get; set; }
+        public string Cedula { get; set; }
+        public string Nombre { get; set; }
+
+        public Persona() { }
+
+        public Persona(int id, string cedula, string nombre) {
+            this.ID = id;
+            this.Cedula = cedula;
+            this.Nombre = nombre;
+        }
+    }
+}
